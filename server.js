6bfe5c203ccd995bbc17f5a0943751f7d7b979c6
Ghaith <ghaithbenhassine@gmail.com>
2021-05-10 13:38:16 +0100

    second commit

--- server.js
+++ server.js
@@ -13,14 +13,34 @@ routes.use(bodyParser.json());
 //cors
 routes.use(cors());
 
+//MongoDb Client
+const MongoClient = require("mongodb").MongoClient;
+const uri =
+    "mongodb+srv://ghaithtest:2440001993Jo@cluster-marketplace.8wrt1.mongodb.net/marketplace?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+// connect to server
 app.listen(PORT, () => {
     console.log(`Server up and running on http://localhost:${PORT}`);
 });
+// connect to DB
+client.connect((err) => {
+    if (err) {
+        throw Error(err);
+    }
+    const collection = client.db("marketplace").collection("products");
+    !err && console.log("Successfully connected to database marketplace");
+    // perform actions on the collection object
+    client.close();
+});
+
+//Routes
 
 routes.get("/", (req, res) => {
     res.send("Hello World!");
 });
-
 routes.get("/products", function (req, res) {
     res.send("liste de produits");
 });
